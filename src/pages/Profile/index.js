7ef3f8802a14bc7d92f6ef28e35b551fda41fc9c
afmelo92 <andre.fabian.melo@gmail.com>
2020-02-25 00:35:23 -0400

    Add update user feature

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useSelector } from 'react-redux';
+import { Form } from '@unform/web';
 
-// import { Container } from './styles';
+import Input from '~/components/Input';
+
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>PROFILE</h1>;
+  const formRef = useRef(null);
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+
+        <hr />
+
+        <Input
+          name="password"
+          type="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input name="password" type="password" placeholder="Sua nova senha" />
+        <Input
+          name="password"
+          type="confirmPassword"
+          placeholder="Confirme nova senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
