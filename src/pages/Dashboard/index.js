77a67e18c6b985346813c056ca007737148448e0
afmelo92 <andre.fabian.melo@gmail.com>
2020-02-26 17:29:17 -0400

    Add main page navigation with dates

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { format, subDays, addDays } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
 import { Container, Time } from './styles';
 
 export default function Dashboard() {
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date]
+  );
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
+
   return (
     <Container>
       <header>
         <button type="button">
-          <MdChevronLeft size={36} color="#FFF" />
+          <MdChevronLeft onClick={handlePrevDay} size={36} color="#FFF" />
         </button>
-        <strong>31 de Fevereiro</strong>
+        <strong>{dateFormatted}</strong>
         <button type="button">
-          <MdChevronRight size={36} color="#FFF" />
+          <MdChevronRight onClick={handleNextDay} size={36} color="#FFF" />
         </button>
       </header>
 
